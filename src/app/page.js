327e024f4e4faa4308dad6f3868c048d1bebb9dd
Maksim Kononyuk <maksimkononyuknaskella@gmail.com>
2024-12-29 15:54:49 +0300

    add image 3

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import Head from "next/head";
+import image from '../../public/mainimage.webp'
 
 export default function Home() {
   return (
@@ -16,7 +16,7 @@ export default function Home() {
           <p>- Text to wats up +38099771243 (time zone - Eastern European Time (EET), which is UTC+2)</p>
           <p className={styles.pEnd}>Need help with academic assignments? I’m here to make it easy! From research to writing, I’ll guide you step by step. Save time, reduce stress, and achieve great results. Let’s tackle your academic challenges together—no more struggle, just success!</p>
         </div>
-        <Image className={styles.image} src='/mainimage.webp' fill style={{objectFit: 'contain'}} alt="Picture of the author"/>
+        <Image className={styles.image} src={image} fill style={{objectFit: 'contain'}} alt="Picture of the author"/>
       </section>
     </main>
     <footer className={styles.footer}>
