d94d6b97e19e4f503348728f2d3c4feec57f9692
Maksim Kononyuk <maksimkononyuknaskella@gmail.com>
2025-01-05 19:03:01 +0300

    added text block

--- src/app/page.js
+++ src/app/page.js
@@ -14,6 +14,7 @@ export default function Home() {
         <div className={styles.textContainer}>
           <div className={styles.topText}>
             <p>- Fast and quality academic assignments</p>
+            <p>- 1 Direct communication with your writer; no frustrating communication to managers unable to understand the essence of your request</p>
             <p>- Text to wats up +38099771243 (time zone - Eastern European Time (EET), which is UTC+2)</p>
           </div>
           <p className={styles.pEnd}>Need help with academic assignments? I’m here to make it easy! From research to writing, I’ll guide you step by step. Save time, reduce stress, and achieve great results. Let’s tackle your academic challenges together—no more struggle, just success!</p>
